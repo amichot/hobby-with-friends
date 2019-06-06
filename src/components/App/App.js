28b7cc13ec948936b730f6cfb8499f2318bbdc4e
amichot <30712077+amichot@users.noreply.github.com>
2019-06-06 00:02:42 -0500

    added create event page html

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import {Route, Switch} from 'react-router-dom';
 import Header from '../Header/Header';
 import LoginPage from '../../routes/LoginPage/LoginPage';
 import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage';
+import CreateEventPage from '../../routes/CreateEventPage/CreateEventPage';
 
 import './App.css';
 
@@ -19,6 +20,7 @@ class App extends Component {
             <Switch>
               <Route path="/login" component={LoginPage} />
               <Route path="/register" component={RegistrationPage} />
+              <Route path="/create" component={CreateEventPage} />
             </Switch>
           }
         </main>
