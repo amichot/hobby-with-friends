07e62576f103a868c8f1d4a6626a3eb01f2d274d
amichot <30712077+amichot@users.noreply.github.com>
2019-06-10 16:28:44 -0500

    changed Nav Bar and moved it to header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 import Header from '../Header/Header';
-import NavigationBar from '../NavigationBar/NavigationBar';
 import LoginPage from '../../routes/LoginPage/LoginPage';
 import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage';
 import HomePage from '../../routes/HomePage/HomePage';
@@ -17,11 +16,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavigationBar />
-        <header className="App__header">
+        <header className="main-content">
           <Header />
         </header>
-        <main className="App__main">
+        <main className="main">
           {
             <Switch>
               <Route path="/login" component={LoginPage} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,46 @@ import './Header.css';
 
 export default function Header() {
   return (
-    <nav className="Header">
-      <h1>
-        {' '}
-        <Link to="/">Hobby with Friends </Link>
-      </h1>
-    </nav>
+    <div className="container-fluid px-lg-5">
+      <nav className="py-4 d-lg-flex">
+        <div id="logo">
+          <h1>
+            {' '}
+            <Link to="/home">Hobby with Friends </Link>
+          </h1>
+        </div>
+        <label htmlFor="drop" className="toggle">
+          Menu
+        </label>
+        <input type="checkbox" id="drop" />
+        <ul className="menu mt-2 ml-auto">
+          <li>
+            <Link className="scroll" to="/home">
+              <span>Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link className="scroll" to="/create">
+              <span>Create Event</span>
+            </Link>
+          </li>
+          <li>
+            <Link className="scroll" to="/search">
+              <span>Search Events</span>
+            </Link>
+          </li>
+          <li>
+            <Link className="scroll" to="/profile">
+              <span>Profile</span>
+            </Link>
+          </li>
+          <li>
+            <Link className="scroll" to="/login">
+              <span>Logout</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
